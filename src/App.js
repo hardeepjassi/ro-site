ed1cdfbd9cf82aab0c1ff1e7edcefb88edb16e31
Hardeep Singh <144507398+hardeepjassi@users.noreply.github.com>
2025-06-20 16:42:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
-import Home from './pages/Home';
+import Home from './pages/home';
 import Review from './pages/Review';
 
 function App() {
